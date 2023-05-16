a579c863e331c1d7fe5e623110d503f78f1cd5c3
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-16 19:21:52 -0300

    feat: test_basic_priority_queueing

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,47 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+mock = [
+    {
+        "nome_do_arquivo": "file1.txt",
+        "qtd_linhas": 10,
+    },
+    {
+        "nome_do_arquivo": "file2.txt",
+        "qtd_linhas": 4,
+    },
+    {
+        "nome_do_arquivo": "file3.txt",
+        "qtd_linhas": 7,
+    },
+    {
+        "nome_do_arquivo": "file3.txt",
+        "qtd_linhas": 3,
+    }
+]
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    instance = PriorityQueue()
+
+    assert instance.is_priority(mock[0]) is False
+    assert instance.is_priority(mock[1]) is True
+    assert instance.is_priority(mock[2]) is False
+    assert instance.is_priority(mock[3]) is True
+
+    instance.enqueue(mock[0])
+    instance.enqueue(mock[1])
+    instance.enqueue(mock[2])
+    instance.enqueue(mock[3])
+        
+    assert len(instance.high_priority) == 2
+    assert len(instance.regular_priority) == 2
+
+    assert instance.search(0) == mock[1]
+
+    instance.dequeue()
+    assert len(instance.high_priority) == 1
+    assert len(instance.regular_priority) == 2
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        instance.search(99)
