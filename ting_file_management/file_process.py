9d12930ec929acf8265aac2ac8bb1c850b5b1535
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-16 14:39:53 -0300

    feat: add def process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    file_out = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file
+    }
+
+    if path_file not in instance.list:
+        instance.enqueue(path_file)
+        sys.stdout.write(str(file_out))
+
 
 
 def remove(instance):
