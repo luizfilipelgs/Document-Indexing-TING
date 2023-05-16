f10e289a9ea0fab12735c2301211c588194e33f4
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-16 16:04:06 -0300

    fix: refactor, Feat: creat_file and file_metadata

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,19 +3,13 @@ from ting_file_management.file_management import txt_importer
 from ting_file_management.queue import Queue
 
 
-
 def process(path_file, instance):
     file = txt_importer(path_file)
-    file_out = {
-        "nome_do_arquivo": path_file,
-        "qtd_linhas": len(file),
-        "linhas_do_arquivo": file
-    }
+    file_out = create_file_out(path_file)
 
     if path_file not in instance.list:
         instance.enqueue(path_file)
         print(file_out, file=sys.stdout)
-
 
 
 def remove(instance):
@@ -23,11 +17,27 @@ def remove(instance):
         print('Não há elementos', file=sys.stdout)
         return None
 
-    file_process = instance.dequeue()
-    path_file = file_process['nome_do_arquivo']
-    print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
-    
+    path_file = instance.dequeue()
+    print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)    
 
 
 def file_metadata(instance, position):
-    
+    if position < 0 or position > instance.__len__():
+        print("Posição inválida", file=sys.stderr)
+        return None
+
+    file_element = instance.search(position)
+    file_out = create_file_out(file_element)
+
+    print(file_out, file=sys.stdout)
+
+
+def create_file_out(path_file):
+    file = txt_importer(path_file)
+    file_out = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file
+    }
+
+    return file_out
