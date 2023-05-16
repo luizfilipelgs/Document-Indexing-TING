cbc2189f041496a2c6209b99096b3d271f712dd8
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-16 20:50:04 -0300

    feat: word search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,41 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    queue_length = len(instance)
+    for queue_index in range(queue_length):
+        data = instance.search(queue_index)
+        if isinstance(data, dict):
+            lines = data.get('linhas_do_arquivo', [])
+            occurrences = []
+            for line_idx, line_content in enumerate(lines, 1):
+                if word.lower() in line_content.lower():
+                    occurrences.append({"linha": line_idx})
+            if occurrences:
+                result.append({
+                    "palavra": word,
+                    "arquivo": data.get('nome_do_arquivo', ''),
+                    "ocorrencias": occurrences
+                })
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    queue_length = len(instance)
+    for queue_index in range(queue_length):
+        data = instance.search(queue_index)
+        if isinstance(data, dict):
+            lines = data.get('linhas_do_arquivo', [])
+            occurrences = []
+            for line_idx, line_content in enumerate(lines, 1):
+                if word.lower() in line_content.lower():
+                    occurrences.append({
+                        "linha": line_idx,
+                        "conteudo": line_content
+                    })
+            if occurrences:
+                result.append({
+                    "palavra": word,
+                    "arquivo": data.get('nome_do_arquivo', ''),
+                    "ocorrencias": occurrences
+                })
+    return result
