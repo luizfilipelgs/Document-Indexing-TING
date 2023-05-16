e3292c95a73f776d0ad08d70a430090a200869a8
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-16 13:47:24 -0300

    feat: add def txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    name_arq = os.path.basename(path_file)
+    ext = os.path.splitext(name_arq)
+    
+    if ext[1] != '.txt':
+        print('Formato inválido', file=sys.stderr)
+    
+    try:
+        with open(path_file, "r") as file:
+            return file.read().split('\n')
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+
+pa = 'requirements.txt'
+print(txt_importer(pa))
